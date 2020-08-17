ae271fdcc43275da947e704e1affe542fa2b5395
Kyle Cameron <kcameron85@gmail.com>
2020-08-17 07:42:45 -0700

    Added classes and Event Listeners to js files

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,34 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+    constructor(missed, phrases, activePhrases) {
+
+    }
+
+    startGame() {
+
+    }
+
+    getRandomPhrase() {
+
+    }
+
+    handleInteraction() {
+
+    }
+
+    removeLife() {
+
+    }
+
+    checkForWin() {
+
+    }
+
+    gameOver() {
+        
+    }
+
+ }
--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+const newGame = new Game();
+const startGameButton = document.getElementById('btn__reset');
+const keyboardButtons = document.getElementsByClassName('key');
+
+startGameButton.addEventListener('click', () => {
+    newGame.startGame();
+});
+
+for (i = 0; i < keyboardButtons.length; i++) {
+    keyboardButtons[i].addEventListener('click', () => {
+        newGame.handleInteraction();
+    });
+}
